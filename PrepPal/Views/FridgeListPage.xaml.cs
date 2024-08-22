52a18fa8dc65bf045f1ce694fdd16a66a96d58f5
benji0295 <58788309+benji0295@users.noreply.github.com>
2024-08-22 14:29:50 -0400

    Enabled adding and delting from grocery list and fridge list

--- PrepPal/Views/FridgeListPage.xaml.cs
+++ PrepPal/Views/FridgeListPage.xaml.cs
@@ -1,9 +1,49 @@
+using PrepPal.Models;
+using PrepPal.ViewModels;
+using System;
+using System.Linq;
+using Microsoft.Maui.Controls;
+
 namespace PrepPal.Views;
 
 public partial class FridgeListPage : ContentPage
 {
+    private FridgeListViewModel _viewModel;
 	public FridgeListPage()
 	{
 		InitializeComponent();
+        _viewModel = BindingContext as FridgeListViewModel;
 	}
+
+    private async void OnAddItemClicked(object sender, EventArgs e)
+    {
+        string result = await DisplayPromptAsync("Add Item", "Enter the name of the grocery item:");
+
+        if (!string.IsNullOrWhiteSpace(result))
+        {
+            _viewModel.FridgeItems.Add(new FridgeItem { Name = result, IsUsed = false });
+        }
+    }
+
+    private async void OnClearListClicked(object sender, EventArgs e)
+    {
+        bool confirm = await DisplayAlert("Clear List", "Are you sure you want to clear the list?", "Yes", "No");
+        if (confirm)
+        {
+            _viewModel.FridgeItems.Clear();
+        }
+    }
+    private async void OnDeleteSelectedClicked(object sender, EventArgs e)
+    {
+        bool confirm = await DisplayAlert("Delete Selected", "Are you sure you want to delete the selected items?", "Yes", "No");
+
+        if (confirm)
+        {
+            var itemsToRemove = _viewModel.FridgeItems.Where(item => item.IsUsed).ToList();
+            foreach (var item in itemsToRemove)
+            {
+                _viewModel.FridgeItems.Remove(item);
+            }
+        }
+    }
 }
--- PrepPal/Views/GroceryListPage.xaml.cs
+++ PrepPal/Views/GroceryListPage.xaml.cs
@@ -1,9 +1,49 @@
+using PrepPal.Models;
+using PrepPal.ViewModels;
+using System;
+using System.Linq;
+using Microsoft.Maui.Controls;	
+
 namespace PrepPal.Views;
 
 public partial class GroceryListPage : ContentPage
 {
+	private GroceryListViewModel _viewModel;
 	public GroceryListPage()
 	{
 		InitializeComponent();
+		_viewModel = BindingContext as GroceryListViewModel; 
+	}
+
+	private async void OnAddItemClicked(object sender, EventArgs e)
+	{
+		string result = await DisplayPromptAsync("Add Item", "Enter the name of the grocery item:");
+
+		if (!string.IsNullOrWhiteSpace(result))
+		{
+			_viewModel.GroceryItems.Add(new GroceryItem { Name = result, IsBought = false });
+		}
+	}
+
+	private async void OnClearListClicked(object sender, EventArgs e)
+	{
+		bool confirm = await DisplayAlert("Clear List", "Are you sure you want to clear the list?", "Yes", "No");
+		if (confirm)
+		{
+			_viewModel.GroceryItems.Clear();
+		}
+	}
+	private async void OnDeleteSelectedClicked(object sender, EventArgs e)
+	{
+		bool confirm = await DisplayAlert("Delete Selected", "Are you sure you want to delete the selected items?", "Yes", "No");
+
+		if (confirm)
+		{
+			var itemsToRemove = _viewModel.GroceryItems.Where(item => item.IsBought).ToList();
+			foreach (var item in itemsToRemove)
+			{
+				_viewModel.GroceryItems.Remove(item);
+			}
+		}
 	}
 }
