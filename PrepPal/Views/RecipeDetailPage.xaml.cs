774489b78443e1cce0a2879cf23902f200720bf0
benji0295 <58788309+benji0295@users.noreply.github.com>
2024-08-22 20:27:39 -0400

    Added add ingredients to grocery list button

--- PrepPal/Views/RecipeDetailPage.xaml.cs
+++ PrepPal/Views/RecipeDetailPage.xaml.cs
@@ -1,14 +1,17 @@
 using PrepPal.Models;
+using PrepPal.ViewModels;
 
 namespace PrepPal.Views;
 
 public partial class RecipeDetailPage : ContentPage
 {
+    private GroceryListViewModel _groceryListViewModel;
+
     public Recipe SelectedRecipe { get; set; }
     public string InstructionsText { get; set; }
     public string IngredientsText { get; set; }
 
-    public RecipeDetailPage(Recipe selectedRecipe)
+    public RecipeDetailPage(Recipe selectedRecipe, GroceryListViewModel groceryListViewModel)
     {
         InitializeComponent();
 
@@ -16,6 +19,21 @@ public partial class RecipeDetailPage : ContentPage
         InstructionsText = string.Join("\n", selectedRecipe.Instructions);
         IngredientsText = string.Join("\n", selectedRecipe.Ingredients);
 
+        _groceryListViewModel = groceryListViewModel;
+
         BindingContext = this;
+    }
+
+    private void OnAddIngredientsToGroceryListClicked(object sender, EventArgs e)
+    {
+        foreach (var ingredient in SelectedRecipe.Ingredients)
+        {
+            // Debug
+            DisplayAlert("Ingredient", ingredient, "OK");
+
+            _groceryListViewModel.GroceryItems.Add(new GroceryItem { Name = ingredient, IsBought = false });
+        }
+
+        DisplayAlert("Success", "Ingredients added to grocery list", "OK");
     }
 }
--- PrepPal/Views/RecipePage.xaml.cs
+++ PrepPal/Views/RecipePage.xaml.cs
@@ -6,12 +6,19 @@ namespace PrepPal.Views;
 
 public partial class RecipePage : ContentPage
 {
+    private RecipeViewModel _recipeViewModel;
+    private GroceryListViewModel _groceryListViewModel;
+
 	public RecipePage()
 	{
 		InitializeComponent();
+
+        _recipeViewModel = new RecipeViewModel();
+        _groceryListViewModel = new GroceryListViewModel();
+
 		BindingContext = new RecipeViewModel();
 	}
-    private void OnRecipeSelected(object sender, SelectionChangedEventArgs e)
+    private async void OnRecipeSelected(object sender, SelectionChangedEventArgs e)
     {
         // Get the selected recipe
         var selectedRecipe = e.CurrentSelection.FirstOrDefault() as Recipe;
@@ -19,11 +26,8 @@ public partial class RecipePage : ContentPage
         // If a recipe is selected, navigate to the RecipeDetailPage
         if (selectedRecipe != null)
         {
-            // Clear the selection (optional)
+            await Navigation.PushAsync(new RecipeDetailPage(selectedRecipe, _groceryListViewModel));
             ((CollectionView)sender).SelectedItem = null;
-
-            // Navigate to the RecipeDetailPage with the selected recipe
-            Navigation.PushAsync(new RecipeDetailPage(selectedRecipe));
         }
     }
 }
