01d66eb76ca354ba2ff1a7f9b715c55fc17279d1
Ben <58788309+benji0295@users.noreply.github.com>
2024-08-27 12:30:45 -0400

    Added swipe to delete on fridge list

--- PrepPal/ViewModels/FridgeListViewModel.cs
+++ PrepPal/ViewModels/FridgeListViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using System.ComponentModel;
 using PrepPal.Models;
 
@@ -12,6 +13,7 @@ namespace PrepPal.ViewModels
     internal class FridgeListViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<FridgeItem> FridgeItems { get; set; }
+        public ICommand DeleteItemCommand { get; set; }
 
         public FridgeListViewModel()
         {
@@ -20,8 +22,23 @@ namespace PrepPal.ViewModels
                 new FridgeItem { Name = "Milk", ExpirationDate = DateTime.Now.AddDays(7), IsExpired = false, IsUsed = false },
                 new FridgeItem { Name = "Eggs", ExpirationDate = DateTime.Now.AddDays(14), IsExpired = false, IsUsed = false },
             };
+
+            DeleteItemCommand = new Command<FridgeItem>(DeleteItem);
+        }
+        
+        private void DeleteItem(FridgeItem item)
+        {
+            if (item != null && FridgeItems.Contains(item))
+            {
+                FridgeItems.Remove(item);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
