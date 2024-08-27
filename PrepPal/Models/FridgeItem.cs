dab8853d73a1625b6e0a763d48f05b5053f024f8
Ben <58788309+benji0295@users.noreply.github.com>
2024-08-27 11:17:54 -0400

    Added strike through to grocery list and fridge list

--- PrepPal/Models/FridgeItem.cs
+++ PrepPal/Models/FridgeItem.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PrepPal.Models
 {
-    public class FridgeItem
+    public class FridgeItem : INotifyPropertyChanged
     {
         public string Name { get; set; }
         public DateTime ExpirationDate { get; set; }
         public bool IsExpired { get; set; }
-        public bool IsUsed { get; set; }
+        private bool _isUsed;
+
+        public bool IsUsed
+        {
+            get => _isUsed;
+            set
+            {
+                if (_isUsed != value)
+                {
+                    _isUsed = value;
+                    OnPropertyChanged(nameof(IsUsed));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- PrepPal/Models/GroceryItem.cs
+++ PrepPal/Models/GroceryItem.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PrepPal.Models
 {
-    public class GroceryItem
+    public class GroceryItem : INotifyPropertyChanged
     {
         public string Name { get; set; }
-        public bool IsBought { get; set; }
+        
+        private bool _isBought;
+
+        public bool IsBought
+        {
+            get => _isBought;
+            set
+            {
+                if (_isBought != value)
+                {
+                    _isBought = value;
+                    OnPropertyChanged(nameof(IsBought));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
