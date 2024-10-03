d16a8977db70118e7ab249cf8e9dd350b38c0c4c
Ben <58788309+benji0295@users.noreply.github.com>
2024-10-03 14:55:01 -0400

    Added global using class

---
+++ PrepPal/GlobalUsing.cs
@@ -0,0 +1,28 @@
+global using System;
+global using System.Linq;
+global using System.Globalization;
+global using System.Windows.Input;
+global using System.ComponentModel;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using System.Runtime.CompilerServices;
+global using System.Collections.ObjectModel;
+
+global using Microsoft.Maui;
+global using Microsoft.Maui.Graphics;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Controls.Hosting;
+global using Microsoft.Maui.Hosting;
+global using Microsoft.Maui.Devices;
+global using Microsoft.Maui.Media;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Maui.Graphics;
+global using Microsoft.Maui.Storage;
+
+global using PrepPal.ViewModels;
+global using PrepPal.Models;
+global using PrepPal.Views;
+global using PrepPal.Data;
+global using PrepPal.Data.CompiledModels;
